145f379ef6fb8f0595f09b43f88850b8f4644d61
Martin J <jerzakm@gmail.com>
2023-08-17 19:14:00 +0200

    repro, init

---
+++ src/lib/components/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core';
+	import Scene from './Scene.svelte';
+	import { SheetObject, Studio, Theatre } from '@threlte/theatre';
+</script>
+
+<Studio enabled={true} />
+<Canvas
+	colorManagementEnabled
+	rendererParameters={{
+		precision: 'highp',
+		antialias: true,
+		logarithmicDepthBuffer: true,
+		powerPreference: 'high-performance'
+	}}
+>
+	<Theatre>
+		<Scene />
+	</Theatre>
+</Canvas>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import App from '$lib/components/App.svelte';
+</script>
+
+<div>
+	<App />
+</div>
+
+<style>
+	:global(body) {
+		margin: 0;
+	}
+
+	div {
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
